9e4f41ffe2efae454a8a4759fab142b558e6383d
MarcelinoBudiman <marcelinobudiman47@gmail.com>
2022-01-02 20:12:15 +0700

    Add transactionController to web

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\UpdateController;
+use App\Http\Controllers\TransactionController;
 use App\Models\Furniture;
 
 /*
