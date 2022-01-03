c2348d3c3966950ef77f143c2f3b46e7db4fcab3
MarcelinoBudiman <marcelinobudiman47@gmail.com>
2022-01-03 19:47:10 +0700

    fixed button sizing @furniture_detail

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -20,16 +20,16 @@
             </div>
         </div>
         <div class="row justify-content-center">
-            <a class="btn btn-primary py-3 px-5 mx-3 col-md-2" style="background-color: {{PRIMARY_COLOR}}" href="{{URL::previous()}}">Previous</a>
+            <a class="btn btn-primary btn-lg text-center mx-3 col-md-2" style="background-color: {{PRIMARY_COLOR}}" href="{{URL::previous()}}">Previous</a>
             @if ($user!=null && $user->role == 'Admin')
-                <a class="btn btn-success py-3 px-5 mx-3 col-md-2" href="/update-furniture-page/{{$furniture->id}}">Update</a>
+                <a class="btn btn-success btn-lg text-center mx-3 col-md-2" href="/update-furniture-page/{{$furniture->id}}">Update</a>
                 <form action="/delete-furniture/{{$furniture->id}}" method="POST" class="col-md-2">
                     {{method_field('delete')}}
                     {{csrf_field()}}
-                    <button class="btn btn-danger mx-3 py-3 px-5 col-md-12">Delete</button>
+                    <button class="btn text-center btn-danger mx-3 btn-lg col-md-12">Delete</button>
                 </form>
             @else
-                <a href="{{$user ? '/add-to-cart/'.$furniture->id : '/login'}}" class="btn btn-primary mx-3 py-3 px-5 col-md-2" style="background-color: {{PRIMARY_COLOR}}">Add to Cart</a>
+                <a href="{{$user ? '/add-to-cart/'.$furniture->id : '/login'}}" class="btn btn-primary mx-3 btn-lg text-center col-md-2" style="background-color: {{PRIMARY_COLOR}}">Add to Cart</a>
             @endif
         </div>
         <div class="text-center my-4">
